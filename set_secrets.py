44be9cba02c6b982f021050fe575aba0e63bff51
VitaminB16 <artemiy.nosov@gmail.com>
2024-02-15 18:17:57 +0000

    feat: Load secrets from GCP secret manager

--- set_secrets.py
+++ set_secrets.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 
+
 def set_secrets():
     """
     Set environment variables from secrets.txt
@@ -9,10 +10,33 @@ def set_secrets():
     KEY1 = VALUE1
     KEY2 = VALUE2
     """
-    with open("secrets.txt") as f:
-        for line in f:
-            key, value = line.strip().split(" = ")
-            os.environ[key] = value
+    try:
+        with open("secrets.txt") as f:
+            for line in f:
+                key, value = line.strip().split(" = ")
+                os.environ[key] = value
+    except FileNotFoundError:
+        print("No secrets.txt file found. Continuing without these secrets.")
+
+
+def set_gcp_secrets(service="python-roh"):
+    from google.cloud import secretmanager
+
+    project = os.environ["PROJECT"]
+    secret_client = secretmanager.SecretManagerServiceClient()
+    secrets = secret_client.list_secrets(
+        request={"parent": f"projects/{project}", "filter": f"labels.{service}:*"}
+    )
+    for secret in secrets:
+        try:
+            value = secret_client.access_secret_version(
+                request={"name": f"{secret.name}/versions/latest"}
+            )
+            os.environ[secret.name.split("/")[-1]] = value.payload.data.decode("utf-8")
+        except FailedPrecondition as err:
+            print(f"Failed to grab secret {secret.name}. Exception: {err}")
+
 
 load_dotenv()
+set_gcp_secrets()
 set_secrets()
