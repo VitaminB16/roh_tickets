7966be43f40b5ccb8a215d11a05e8fffd4859910
VitaminB16 <artemiy.nosov@gmail.com>
2024-02-18 00:39:43 +0000

    feat: Deploy GCP scheduler job to run once a day to update the data

--- deploy.sh
+++ deploy.sh
@@ -21,3 +21,37 @@ gcloud functions deploy $CLOUD_FUNCTION_NAME \
   --env-vars-file .env.yaml \
   --timeout=120s \
   --memory=1024MB
+
+# Deploy the Cloud Scheduler jobs
+
+# Job 1: Run events task daily at 13:00 GMT to update the events data for any new events and productions
+
+# Job configuration
+JOB_NAME="python-roh-update-events-daily"
+LOCATION="europe-west2"
+URI="https://europe-west2-vitaminb16.cloudfunctions.net/python-roh"
+SCHEDULE="0 13 * * *"
+TIME_ZONE="GMT"
+PAYLOAD="{'task_name': 'events', 'no_plot': true}"
+OIDC_SERVICE_ACCOUNT_EMAIL="vitaminb16@vitaminb16.iam.gserviceaccount.com"
+
+configure_job() {
+  gcloud scheduler jobs $1 http $JOB_NAME \
+    --location $LOCATION \
+    --schedule "$SCHEDULE" \
+    --time-zone "$TIME_ZONE" \
+    --uri "$URI" \
+    --http-method POST \
+    --message-body "$PAYLOAD" \
+    --oidc-service-account-email $OIDC_SERVICE_ACCOUNT_EMAIL
+}
+
+# If the job exists, use update, otherwise use create
+if gcloud scheduler jobs describe $JOB_NAME --location $LOCATION &>/dev/null; then
+  ACTION="update"
+else
+  ACTION="create"
+fi
+
+echo "${ACTION} the job..."
+configure_job $ACTION
