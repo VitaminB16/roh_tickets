0b8d88f6121f3f82ecf5f3cfda1430061531cdf0
VitaminB16 <artemiy.nosov@gmail.com>
2024-02-22 18:53:26 +0000

    feat: Improved Firestore functionality

--- cloud/utils.py
+++ cloud/utils.py
@@ -3,6 +3,7 @@ import json
 import logging
 import requests
 import google.auth
+import concurrent.futures
 import google.auth.exceptions
 from google.oauth2 import id_token
 from google.cloud import pubsub_v1, firestore
@@ -64,54 +65,83 @@ class GCPRequest:
 
 
 class Firestore:
-    """
-    Class for managing documents in Google Cloud Firestore
-    """
-
-    def __init__(self, path=None, project_id=None):
-        self.project_id = project_id or os.getenv("PROJECT")
-        self.client = firestore.Client(self.project_id)
+    def __init__(self, path=None, project=None):
+        self.project = project or os.getenv("PROJECT")
+        self.client = firestore.Client(self.project)
         self.path = path
 
-    def _parse_path(self, method="get"):
-        """
-        Parse the path into collections and documents.
-        Has to be of the form "collection/document/collection/document/.../collection/document"
-        """
+    def parse_path(self, method="get"):
+        if "gs://" in self.path:
+            # gs://project/bucket/path -> project, bucket, path
+            # path -> collection/document/../collection/document
+            self.path = self.path.replace("gs://", "")
+            self.project, self.bucket, self.path = self.path.split("/", 2)
         path_elements = self.path.split("/")
-        collections = path_elements[0::2]
-        documents = path_elements[1::2]
-        if method in ["set", "get"] and len(collections) != len(documents):
-            raise ValueError("Path must have an even number of elements")
-        return collections, documents
+        if method in ["set", "delete"]:
+            raise ValueError("Firestore path must be a document or collection.")
+        return path_elements
 
-    def get_doc_ref(self):
-        """
-        Get a reference to the document.
-        """
-        collections, documents = self._parse_path(self.path)
-        doc_ref = self.client.collection(collections[0]).document(documents[0])
-        for collection, document in zip(collections[1:], documents[1:]):
-            doc_ref = doc_ref.collection(collection).document(document)
+    def get_ref(self, method=None):
+        path_elements = self.parse_path(method)
+        doc_ref = self.client.collection(path_elements.pop(0))
+        ref_type = "document"
+        while len(path_elements) > 0:
+            doc_ref = getattr(doc_ref, ref_type)(path_elements.pop(0))
+            ref_type = "document" if ref_type == "collection" else "collection"
+        print(f"Firestore {ref_type} reference: {self.path}")
         return doc_ref
 
     def get(self):
-        """
-        Read from Firestore. E.g. Firestore("collection/document").get()
-        """
-        doc_ref = self.get_doc_ref()
+        doc_ref = self.get_ref(method="get")
         output = doc_ref.get().to_dict()
         print(f"Read from Firestore: {self.path}")
         return output
 
     def set(self, data):
-        """
-        Write to Firestore. E.g. Firestore("collection/document").set(data)
-        """
-        doc_ref = self.get_doc_ref()
+        doc_ref = self.get_ref(method="set")
         doc_ref.set(data)
         print(f"Written to Firestore: {self.path}")
         return True
+
+    def delete(self):
+        """
+        Recursively deletes documents and collections from Firestore.
+        """
+        ref = self.get_ref()
+        ref_type = self.ref_type(ref)
+        if ref_type == "document":
+            self.delete_document(ref)
+        elif ref_type == "collection":
+            self.delete_collection(ref)
+        else:
+            raise ValueError("Unsupported Firestore reference type.")
+        print(f"Deleted {ref_type} from Firestore: {self.path}")
+        return True
+
+    def delete_document(self, doc_ref):
+        """
+        Deletes a document and all of its collections.
+        """
+        collections = doc_ref.collections()
+        for collection in collections:
+            self.delete_collection(collection)
+        doc_ref.delete()
+
+    def delete_collection(self, col_ref):
+        """
+        Deletes a collection and all of its documents.
+        """
+        docs = col_ref.stream()
+        with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
+            futures = [
+                executor.submit(self.delete_document, doc.reference) for doc in docs
+            ]
+            concurrent.futures.wait(futures)
+
+    def ref_type(self, doc_ref):
+        is_doc_ref = isinstance(doc_ref, firestore.DocumentReference)
+        is_coll_ref = isinstance(doc_ref, firestore.CollectionReference)
+        return "document" if is_doc_ref else "collection" if is_coll_ref else None
 
 
 class PubSub:
