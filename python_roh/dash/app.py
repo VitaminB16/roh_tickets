630b4579253e31ade272ae8849a14624fa64aee9
VitaminB16 <artemiy.nosov@gmail.com>
2024-02-27 00:45:02 +0000

    enh: Fixed app font, added repo link

--- python_roh/dash/app.py
+++ python_roh/dash/app.py
@@ -3,6 +3,7 @@ import darkdetect
 import pandas as pd
 from dash import dcc, html, no_update
 from dash.exceptions import PreventUpdate
+from dash_svg import Svg, G, Path, Circle
 from dash.dependencies import Input, Output, State
 
 
@@ -25,6 +26,27 @@ TOGGLE_BUTTON_STYLE = {
 }
 AUTO_MARGIN = {"marginLeft": "auto", "marginRight": "auto"}
 FONT_FAMILY = {"font-family": "Gotham SSm, Futura, Roboto, Arial, Lucida Sans"}
+GITHUB_LOGO_SVG_PATH = "M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"
+
+global DASH_PAYLOAD_DEFAULTS
+DASH_PAYLOAD_DEFAULTS = {
+    "dont_save": True,  # don't save the plot?
+    "save_both": False,  # save both light and dark mode?
+    "dont_show": True,  # don't show the plot?
+    "show_both": False,  # show both light and dark mode?
+    "dark_mode": not is_dark_mode,  # dark mode?
+    "query_events_api": False,  # query the events API?
+    "print_performance_info": False,  # print performance info?
+    "autosize": True,  # autosize parameter
+    "font_family": "Gotham SSm",  # font family
+}
+
+
+def get_url_style(is_dark_mode):
+    return {
+        "color": "#FA8072" if is_dark_mode else "#0000EE",
+        "textDecoration": "underline",
+    }
 
 
 # Helper Functions
@@ -41,18 +63,6 @@ def update_toggle_button_style(is_dark_mode):
     )
     return {**base_style, **mode_style}
 
-
-global DASH_PAYLOAD_DEFAULTS
-DASH_PAYLOAD_DEFAULTS = {
-    "dont_save": True,  # don't save the plot?
-    "save_both": False,  # save both light and dark mode?
-    "dont_show": True,  # don't show the plot?
-    "show_both": False,  # show both light and dark mode?
-    "dark_mode": not is_dark_mode,  # dark mode?
-    "query_events_api": False,  # query the events API?
-    "print_performance_info": False,  # print performance info?
-    "autosize": True,  # autosize parameter
-}
 
 app.layout = html.Div(
     [
@@ -78,6 +88,7 @@ def update_dynamic_content(theme_data):
 
 def serve_layout(dark_mode):
     style = create_dynamic_style(dark_mode)
+    url_style = get_url_style(dark_mode)
 
     full_page_style = {
         "minHeight": "100vh",
@@ -95,6 +106,49 @@ def serve_layout(dark_mode):
                 "Toggle Dark/Light Mode",
                 id="dark-mode-toggle",
                 style=update_toggle_button_style(dark_mode),
+            ),
+            html.Div(
+                [
+                    html.Div(  # GitHub logo
+                        html.A(
+                            Svg(
+                                G(Path(d=GITHUB_LOGO_SVG_PATH)),
+                                width=32,
+                                height=32,
+                                viewBox="0 0 16 16",
+                                fill="#000000" if not dark_mode else "#FFFFFF",
+                            ),
+                            href="https://github.com/VitaminB16/roh_tickets",
+                            target="_blank",
+                            style={
+                                "display": "inline-block",
+                                "vertical-align": "middle",
+                            },
+                        ),
+                        style={"display": "inline-block"},
+                    ),
+                    html.Div(  # GitHub repo link
+                        html.A(
+                            "Source code",
+                            href="https://github.com/VitaminB16/roh_tickets",
+                            target="_blank",
+                            style={
+                                "display": "inline-block",
+                                "vertical-align": "middle",
+                                "marginLeft": "5px",
+                                **url_style,
+                            },
+                        ),
+                        style={"display": "inline-block"},
+                    ),
+                ],
+                style={
+                    "position": "absolute",
+                    "top": "20px",
+                    "right": "10px",
+                    "zIndex": "1000",
+                    **FONT_FAMILY,
+                },
             ),
             html.H1(
                 "Events and Seats Dashboard",
@@ -259,8 +313,7 @@ def display_seats_map(clickData, _, theme_data):
         "display": "flex",
         "flexDirection": "column",
         "alignItems": "right",
-        "color": "#FA8072" if theme_data["dark_mode"] else "#0000EE",
-        "textDecoration": "underline",
+        **get_url_style(theme_data["dark_mode"]),
     }
 
     event_info_box = html.Div(
