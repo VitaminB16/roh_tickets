0b35ef6dffcaa9b9e03ed3e8c84296dfadbc4f69
VitaminB16 <artemiy.nosov@gmail.com>
2024-03-11 01:35:35 +0000

    feat: Extract text information from web page

--- various/seat_map_positions/extract_positions.js
+++ various/seat_map_positions/extract_positions.js
@@ -5,9 +5,14 @@ const zones = [
   { selector: '#seatmap #Grand-Tier .seat', name: 'Donald Gordon Grand Tier' },
   { selector: '#seatmap #Balcony .seat', name: 'Balcony' },
 ];
+const text = [
+  { selector: 'text.st3.st4.st6', type: 'Text' },
+  { selector: 'text.st3.st4.st5', type: 'Text' },
+]
 
 let records = [];
 
+// Process the seats information on the page
 zones.forEach(zone => {
   const seats = document.querySelectorAll(zone.selector);
   seats.forEach(seat => {
@@ -38,6 +43,31 @@ zones.forEach(zone => {
         cy,
         ZoneName: zone.name,
         id: seat.id,
+      };
+      records.push(record);
+    }
+  });
+});
+
+// Process the text information on the page
+text.forEach(textInfo => {
+  const textElements = document.querySelectorAll(textInfo.selector);
+  textElements.forEach(textEl => {
+    let cx, cy, textCont;
+    const transform = textEl.getAttribute('transform');
+    const matrixMatch = /matrix\(\d\s*\d\s*\d\s*\d\s*([-\d.]+)\s*([-\d.]+)\)/.exec(transform);
+    if (matrixMatch) {
+      [cx, cy] = [matrixMatch[1], matrixMatch[2]];
+    }
+    textCont = textEl.textContent
+
+    // Ensure that the text contains at least one letter
+    if (cx !== undefined && cy !== undefined && /[a-zA-Z]/.test(textCont)) {
+      const record = {
+        cx,
+        cy,
+        ZoneName: textCont,
+        id: textInfo.type,
       };
       records.push(record);
     }
